47c80acb2280d1dfc193f5bd9f621bda0f861eb0
WIN-IVGQMH69UIV\Administrator <wukunling192@hfjy.com>
2018-10-08 18:43:57 +0800

    demo

--- src/Gantt.vue
+++ src/Gantt.vue
@@ -1,163 +1,220 @@
 <template>
   <div id='tableid'>
-    <table class="hello">
+    <div class="header">{{new Date().toJSON()}}</div>
+    <div class="top"><span class="month" v-for="(item,index) in month" :key='index' @click="switchTo(item)">{{item}}</span></div>
+    <table class="hello" >
       <tr>
-        <th v-for="(item, index) in date" :key='index'>
-          <span v-if="index=='0'"></span>
-          <span v-else>{{item.day}}</span>
+        <th>
+          <span >kk</span>
+        </th>
+        <th v-for="(item, index) in headerDays" :key='index'>
+          <span >{{item.days}}</span>
         </th>
       </tr>
-      <tr v-for="(items,indexs) in opt" :key='indexs'>
-        <td v-for="(item, index) in newDate" :key='index'>
-          <a class='isActive' v-if="item.day==isActive">dscds</a>
-          <span v-if="index=='0'" >{{items.title}}</span>
-          <span v-else :data-x='item.day' :data-y='indexs+1'><a :style="{ background: item.hh.activeColor,width: item.hh.width}" :data='item.hh.remark' @mouseenter="mouseenter" @mouseleave="mouseleave" v-if="item.day===item.hh.today&&item.hh.y===indexs"  class="iniClass">{{item.hh.title}} {{item.hh.sum}} %</a><a :data='item.hh.remark' v-if="item.day===item.hh.today&&item.hh.y===indexs" @mouseenter="mouseenter" @mouseleave="mouseleave" class="iniClassB" :style="{ background: item.hh.activeColors,width: item.hh.widths}">''</a></span>
+      <tr v-for="(item, index) in data" :key='index'>
+        <td>
+          <span>{{item.title}}</span>
+        </td>
+        <td v-for="(ite, ins) in item.days" :key='ins'>
+          <span>
+            <a v-if="ite.title&&ite.width!=='0'" :data='ite.remark' class="iniClass" @mouseenter="mouseenter" @mouseleave="mouseleave" :style="{ background: ite.activeColor,width: ite.width}">{{ite.title}} {{ite.percentum}}</a>
+            <a v-if="ite.title" :data='ite.remark' @mouseenter="mouseenter" @mouseleave="mouseleave" :style="{ background: ite.activeColors,width: ite.widths}" class="iniClassB"></a>
+            <a v-if="ite.isweek" :data='ite.remark'  class="iniClassC"></a>
+          </span>
         </td>
       </tr>
     </table>
-    <div class="tooltip" :style="{ top: top,left:left}" v-if="isshow"><p>{{remark}}</p><div class="arrow"></div></div>
+    <div class="tooltip" :style="{ top: top,left:left}" v-if="isshow">
+      <p>{{remark}}</p>
+      <div class="arrow"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getDays } from './time.js'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      opt: [
-        {
-          title: '项目一',
-          today: '2018-9-1',
-          endTime: '2018-9-9',
-          activeColor: 'red',
-          activeColors: 'green',
-          remark :'这是项目一'
-        },
-        {
-          title: '项目二',
-          today: '2018-9-2',
-           endTime: '2018-9-9',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark :'这是项目二'
-        },
-        {
-          title: '项目三',
-          today: '2018-9-3',
-           endTime: '2018-9-9',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目三'
-        },
-        {
-          title: '项目四',
-          today: '2018-9-4',
-           endTime: '2018-9-10',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目四'
-        },
-         {
-          title: '项目五',
-          today: '2018-9-5',
-           endTime: '2018-9-15',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目五'
-        },
-         {
-          title: '项目六',
-          today: '2018-9-6',
-           endTime: '2018-9-15',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目六'
-        },
-        {
-          title: '项目七',
-          today: '2018-9-7',
-           endTime: '2018-9-15',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目七'
-        },
-         {
-          title: '项目八',
-          today: '2018-9-12',
-           endTime: '2018-9-18',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目八'
-        },
-        {
-          title: '项目九',
-          today: '2018-9-9',
-           endTime: '2018-9-29',
-          activeColor: 'red',
-          activeColors: 'green',
-           remark : '这是项目九'
-        },
-         {
-          title: '项目十',
-          today: '2018-9-8',
-          endTime: '2018-9-15',
-          activeColor: 'red',
-          activeColors: 'green',
-          remark : '这是项目十'
-        }
-      ],
-      opts: [],
-      date: [],
-      dates: [],
-      newDate: [],
+      row: [{
+        title: '项目一',
+        today: '2018-08-9',
+        endTime: '2018-10-20',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目一'
+      },
+      {
+        title: '项目二',
+        today: '2018-10-9',
+        endTime: '2018-10-16',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      },
+      {
+        title: '项目三',
+        today: '2018-10-15',
+        endTime: '2018-10-25',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      },
+      {
+        title: '项目四',
+        today: '2018-10-3',
+        endTime: '2018-10-14',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目五',
+        today: '2018-10-18',
+        endTime: '2018-10-25',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      },
+      {
+        title: '项目六',
+        today: '2018-10-3',
+        endTime: '2018-10-9',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-3',
+        endTime: '2018-10-15',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-3',
+        endTime: '2018-10-9',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-3',
+        endTime: '2018-10-9',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-3',
+        endTime: '2018-10-24',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-13',
+        endTime: '2018-10-28',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-23',
+        endTime: '2018-10-17',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }, {
+        title: '项目六',
+        today: '2018-10-14',
+        endTime: '2018-10-19',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      },
+      {
+        title: '项目六',
+        today: '2018-10-10',
+        endTime: '2018-10-20',
+        activeColor: 'red',
+        activeColors: 'green',
+        remark: '这是项目二'
+      }],
+      rows: [],
+      month: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+      headerDays: [],
+      data: [],
       isshow: false,
       top: '0',
       left: '0',
-      remark: '',
-      isActive:new Date().getFullYear() + `${'-'}` + parseInt(new Date().getMonth() + 1) + `${'-'}` + new Date().getDate()
-
+      remark: ''
     }
   },
   created () {
-    this.tableDay()
+    this.switchTo()
   },
   methods: {
-    tableDay () {
-      this.opt.map((ite, ins) => {
-        ite.y = ins
-      })
-      let days =
-      this.mGetDate(new Date().getFullYear(), new Date().getMonth()) + 1
-      for (let index = 0; index < days; index++) {
-       this.date.push({
-          day: new Date().getFullYear() + `${'-'}` + parseInt(new Date().getMonth() + 1) + `${'-'}` + parseInt(index),
-          hh: {}
-        })
+    headerTime (arr) {
+      // 重置
+      this.data = []
+      this.headerDays = []
+
+      // 当前月的计算
+      for (let index = 0; index < getDays(); index++) {
+        let dayDate = new Date().getFullYear() + `${'-'}` + parseInt(new Date().getMonth() + 1) + `${'-'}` + parseInt(index + 1)
+        this.headerDays.push({dates: dayDate, days: index + 1})
       }
-      this.date.map((item, indexs) => {
-        this.dates.push({ ...item,hh: this.opt[indexs] || { today: '' } })
-      })
-      this.dates.map((items, indexs) => {
-        let hh = {}
-        this.dates.map((itemss, indexss) => {
-          if (items.day === itemss.hh.today) {
-            console.log(items.day,itemss.hh.today)
-            let datediff=this.DateDiff(itemss.hh.today,itemss.hh.endTime).percent
-            let sum=this.DateDiff(itemss.hh.today,itemss.hh.endTime).sum/86400
-            let nowSum=this.DateDiff(itemss.hh.today,itemss.hh.endTime).nowSum/86400
-            hh = { ...itemss.hh }
-            hh.sum = datediff.toFixed(2)
-            hh.width=sum*83+'px'
-            hh.widths=nowSum*83+'px'
+      arr.map((ite, ins) => {
+        let obg = {
+          days: []
+        }
+        this.headerDays.forEach((element, index) => {
+          // 初始化数据结构
+          let daysObj = {
+            title: '',
+            today: '',
+            endTime: '',
+            activeColor: '',
+            activeColors: '',
+            remark: '',
+            widths: '',
+            width: ''
+          }
+          if (this.isWeek(element.dates)) {
+            // 周六 周如处理
+            daysObj.isweek = true
+          }
+          if (ite.today === element.dates) {
+            // console.log(ite)
+            // 构造数据结构
+            let sum = (this.timestamp(ite.endTime) - this.timestamp(ite.today)) / 86400000
+            let now = (this.timestamp(new Date()) - this.timestamp(ite.today)) / 86400000
+            ite.widths = sum * 40 + 'px'
+            ite.width = now * 40 + 'px'
+            ite.percentum = (now / sum).toFixed(2) + '%'
+            daysObj.day = element.dates
+            daysObj = ite
+            obg.title = ite.title
+            if (now <= 0) {
+              daysObj.remark = '当前项目还未开展'
+              // daysObj.title = "''"
+              daysObj.width = '0'
+            }
+            obg.days.push(daysObj)
+          } else {
+            obg.days.push(daysObj)
           }
         })
-        let obj = { day: items.day, hh: hh }
-        this.newDate.push(obj)
+        this.data.push(obg)
       })
     },
     // 鼠标事件
     mouseenter (e) {
-      this.remark= e.target.getAttribute('data')
+      this.remark = e.target.getAttribute('data')
+      if (e.target.title) {
+        this.isshow = false
+      }
       this.isshow = true
       this.left = e.clientX - 70 + 'px'
       this.top = e.clientY - 90 - e.offsetY / 2 + 'px'
@@ -165,19 +222,30 @@ export default {
     mouseleave (e) {
       this.isshow = false
     },
-    // 工具函数   时间
-    mGetDate () {
-      var date = new Date()
-      var year = date.getFullYear()
-      var month = date.getMonth() + 1
-      var d = new Date(year, month, 0)
-      return d.getDate()
+    switchTo (e) {
+      let arr = []
+      let month
+      if (e) {
+        month = new Date().getFullYear() + '-' + e
+      } else {
+        month = new Date().toJSON().slice(0, 7)
+      }
+      this.row.map(item => {
+        if (item.today.includes(month)) {
+          arr.push(item)
+        }
+      })
+      this.headerTime(arr)
     },
-    DateDiff (sDate1, sDate2) {
-      let sum=new Date(sDate2).getTime()/1000-new Date(sDate1).getTime()/1000
-      let nowSum=parseInt(new Date().getTime()/1000) -new Date(sDate1).getTime()/1000
-      let percent=nowSum/sum
-      return {percent:percent,sum:sum,nowSum:nowSum}
+    // 工具函数库
+    timestamp (time) {
+      return new Date(time || null).getTime()
+    },
+    isWeek (Week) {
+      var dt = new Date(Week || null)
+      if (dt.getDay() === 6 || dt.getDay() === 0) {
+        return true
+      }
     }
   }
 }
@@ -188,25 +256,45 @@ export default {
 .hello {
   position: absolute;
 }
+
 table {
-  width: 200%;
+  /* width: 100%; */
   border-collapse: collapse;
 }
 
 table,
 td,
 th {
+  min-width: 40px;
+  height: 50px;
   position: relative;
-  padding: 10px 5px 17px 7px;
-  border: 1px solid black;
+  /* padding: 10px 5px 17px 7px; */
+  border: 1px solid #00000029;
 }
+
 table,
 td,
 th,
 span {
+  width: 100%;
   position: relative;
+  font-family: sans-serif;
+  font-size: 14px;
+  font-weight: 300;
 }
-.iniClassB {
+.iniClass{
+  cursor: pointer;
+  /* width: 100px; */
+   height: 20px;
+  border-radius: 5px;
+  font-size: 3px;
+  position: absolute;
+  z-index: 3;
+  color: #fff
+}
+.iniClassB{
+  width: 20px;
+    height: 20px;
   cursor: pointer;
   display: inline-block;
   font-size: 3px;
@@ -214,17 +302,11 @@ span {
   border-radius: 5px;
   z-index: 1;
 }
-.iniClass {
-  cursor: pointer;
-  width: 100px;
-  border-radius: 5px;
-  font-size: 3px;
-  position: absolute;
-  z-index: 3;
+
+#tableid {
+  position: relative;
 }
-#tableid{
-  position: relative
-}
+
 #container {
   width: 400px;
   margin: 100px auto 0 auto;
@@ -287,9 +369,30 @@ h1 {
   border-right: 10px solid transparent;
   border-top: 10px solid #fff;
 }
-
-.isActive{
-  
-  background-color: red;
+.iniClassC{
+  top: 1px;
+    left: 1px;
+    background-color: #00ffff4d;
+    width: 42px;
+    height: 52px;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 3px;
+    position: absolute;
+    z-index: 1;
+}
+.header{
+  padding: 15px 0;
+  text-align: center;
+}
+.month{
+  width: 115px;
+  padding: 15px 0;
+  display: inline-block;
+  cursor: pointer
+}
+.top{
+  border-top: 1px solid #00000029;
+  /* border: 1px solid #00000029; */
 }
 </style>
